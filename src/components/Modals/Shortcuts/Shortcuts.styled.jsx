ba9a48ca208a4ca5b0471d557d8cded492710741
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-07-20 17:48:08 +0300

    +

--- src/components/Modals/Shortcuts/Shortcuts.styled.jsx
+++ src/components/Modals/Shortcuts/Shortcuts.styled.jsx
@@ -21,26 +21,28 @@ export const StyledText = styled.p`
   color: rgba(58, 53, 65, 0.6);
 `;
 
-export const StyledButton = styled(ListItemButton)`
-  width: 130px;
-  height: 40px;
-  background-color: ${props => (props.selected ? '#F7F2FF' : 'transparent')};
-`;
+// export const StyledButton = styled(ListItemButton)`
+//   width: 130px;
+//   height: 40px;
+//   background-color: ${props => (props.selected ? '#F7F2FF' : 'transparent')};
+// `;
 
-export const StyledListItem = styled(ListItemText)`
-  margin: 0;
-  font-family: 'Inter';
-  font-size: 16px;
-  font-weight: 500;
-  color: rgba(58, 53, 65, 0.87);
-`;
+// export const StyledListItem = styled(ListItemText)`
+//   margin: 0;
+//   font-family: 'Inter';
+//   font-size: 16px;
+//   font-weight: 500;
+//   color: rgba(58, 53, 65, 0.87);
+// `;
 
 export const StyledLink = styled(Link)`
+  width: 174px;
+  height: 151px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 24px;
+  // padding: 24px;
 
   :hover,
   focus {
@@ -52,8 +54,6 @@ export const StyledContainerIcon = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 50px;
-  height: 50px;
   border-radius: 50%;
   margin-bottom: 8px;
   background-color: #f0f1f2;
@@ -75,6 +75,7 @@ export const StyledTextTitle = styled.p`
 
 export const StyledTextCart = styled.p`
   margin: 0;
+  text-align: center;
   font-family: 'Inter';
   font-size: 14px;
   font-weight: 500;
